1bfe0d23b8f84a673c9790996a5b9a550486e956
darambae <64197421+jazzy1101@users.noreply.github.com>
2023-08-24 22:25:42 +0200

    c01

---
+++ ex03/ft_div_mod.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: darambae <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 20:21:36 by darambae          #+#    #+#             */
+/*   Updated: 2023/08/24 20:26:57 by darambae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+int		main(void)
+{
+	int a;
+	int b;
+	int div;
+	int mod;
+
+	a = 16;
+	b = 5;
+	ft_div_mod(a, b, &div, &mod);
+	if (div == 3 && mod == 1)
+	{
+		write(1, "OK\n", 3);
+	}
+	else
+	{
+		write(1, "FAIL\n", 5);
+	}
+}
---
+++ ex07/ft_rev_int_tab.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: darambae <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 21:25:18 by darambae          #+#    #+#             */
+/*   Updated: 2023/08/24 21:38:35 by darambae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int emp_arr[size];
+	int	i;
+	i = size - 1;
+	while (i >= 0)
+	{
+		emp_arr[i - size -1] = *tab[i];
+		i--;
+	}
+}
+
+int		main(void)
+{
+	int tab[10] = {0,1,2,3,4,5,6,7,8,9};
+
+	printf("9,8,7,6,5,4,3,2,1,0:");
+	ft_rev_int_tab(tab, 10);
+	printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5], tab[6], tab[7], tab[8], tab[9]);
+}
